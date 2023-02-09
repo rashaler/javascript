42bdeb798d70c51ca845597fa168adfa0e6d9f0b
Richard Shaler <rashaler@hotmail.com>
2023-02-09 09:52:25 -0800

    Add import all

--- ES6-vs-CommonJS/es6-app.js
+++ ES6-vs-CommonJS/es6-app.js
@@ -1,5 +1,10 @@
 // app.js 
-import { hello1, hello2 } from './es6-hello.js'
+// import { hello1, hello2 } from './es6-hello.js'
+// --or--
+import * as myExports from './es6-hello.js'
 
-console.log(hello1()) // returns hello1
-console.log(hello2())  // returns hello2
+// console.log(hello1()) // returns hello1
+// console.log(hello2())  // returns hello2
+
+console.log(myExports.hello1()) // returns hello1
+console.log(myExports.hello2())  // returns hello2
