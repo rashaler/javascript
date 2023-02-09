7f43d20af9021a9ae901c5aa69f35dd094495db8
Richard Shaler <rashaler@hotmail.com>
2023-02-09 09:44:06 -0800

    Add modulesexample

---
+++ ES6-vs-CommonJS/es6-app.js
@@ -0,0 +1,5 @@
+// app.js 
+import { hello1, hello2 } from './es6-hello.js'
+
+console.log(hello1()) // returns hello1
+console.log(hello2())  // returns hello2
---
+++ ES6-vs-CommonJS/es6-hello.js
@@ -0,0 +1,9 @@
+// hello.js 
+function hello1() {
+    return 'hello1'
+}
+function hello2() {
+    return 'hello2'
+}
+
+export { hello1, hello2 }
